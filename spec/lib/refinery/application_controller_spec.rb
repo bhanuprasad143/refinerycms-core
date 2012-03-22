b5e6617296ea1354cfe80e4de374cd6dd81ede94
Peter Mellon <peter.mellon@locomotivellc.com>
2012-03-22 11:16:20 +0700

    initial commit

---
+++ spec/lib/refinery/application_controller_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe "Refinery::ApplicationController" do
+  describe "DummyController", :type => :controller do
+    controller do
+      include ::Refinery::ApplicationController
+    end
+
+    describe ".home_page?" do
+      it "matches root url" do
+        controller.stub(:root_path).and_return("/")
+        request.stub(:path).and_return("/")
+        controller.home_page?.should be_true
+      end
+
+      it "matches localised root url" do
+        controller.refinery.stub(:root_path).and_return("/en/")
+        request.stub(:path).and_return("/en")
+        controller.home_page?.should be_true
+      end
+
+      it "escapes regexp" do
+        request.stub(:path).and_return("\/huh)")
+        expect { controller.home_page? }.to_not raise_error(RegexpError)
+      end
+    end
+  end
+end
